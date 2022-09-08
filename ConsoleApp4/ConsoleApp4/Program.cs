980b9c2e4b6e6a91d026314ac2699ceba6ae11ff
yousoezs <skorotoni@hotmail.com>
2022-09-08 10:42:59 +0200

    Bubble sort

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[] { 1, 3, 2, 5, 4, 9 };
+
+            int temp = 0;
+
+            for(int write = tal.Length - 1; write >= 0; write--)
+            {
+                for (int sort = 0; sort < tal.Length - 1; sort++)
+                {
+                    if (tal[sort] > tal[sort + 1])
+                    {
+                        temp = tal[sort + 1];
+                        tal[sort + 1] = tal[sort];
+                        tal[sort] = temp;
+                    }
+                }
+                WriteLine($"Sorterat: {tal[write]}");
+            }
+        }
+    }
+}
