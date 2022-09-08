696ebd0c70b94c916aee8d2b22772c6b08dba79a
yousoezs <skorotoni@hotmail.com>
2022-09-08 18:46:11 +0200

    Uppgift 18

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        //Be användaren mata in en mening. Skriv ut det genomsnittliga antalet bokstäver i orden.
+        static void Main(string[] args)
+        {
+            WriteLine("Type in a text!");
+            string userInput = ReadLine();
+            string[] words = userInput.Split(' ');
+            if (userInput.Contains(" "))
+            {
+                userInput.Replace(" ", "");
+            }
+            double result = userInput.Length / words.Length;
+
+
+            WriteLine($"Det genomsnittliga antalet bokstäver är: {result}");
+        }
+    }
+}
