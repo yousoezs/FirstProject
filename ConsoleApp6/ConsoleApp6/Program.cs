207765f5bc1734a4516595bfa3cde5b6776fcef4
yousoezs <skorotoni@hotmail.com>
2022-09-11 00:05:07 +0200

    Uppgift 19

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(myInt(4,8));
+        }
+        static int myInt(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Metrics;
+using System.Drawing;
+using static System.Console;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        //Skriv en metod DrawBox(int width, int height)
+        //När man anropar metoden ska den tömma konsolen och skriva ut en rektangel där de yttre tecknen består av ‘#’ och de inre av ‘-’ Exempel:
+        //DrawBox(7, 4);
+        //#######
+        //#-----#
+        //#-----#
+        //#######
+        static void Main(string[] args)
+        {
+            DrawBox(7, 4);
+        }
+        static int DrawBox(int width, int height)
+        {
+            Clear();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    int length = height - 1;
+                    if (i == 0 || i == length)
+                    {
+                        Write("#");
+                    }
+                    else if (j == 0 || j == width - 1)
+                        Write("#");
+                    else
+                        Write("-");
+                    if (j == width - 1)
+                    {
+                        Write("\n");
+                    }
+                }
+            }
+            return height;
+            return width;
+        }
+    }
+}
