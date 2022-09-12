3b0ee5063cf0fa51b87f7095fbed27de82255a06
yousoezs <skorotoni@hotmail.com>
2022-09-12 13:41:47 +0200

    Klasser, property

---
+++ ConsoleApp8/ConsoleApp8/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Car
+    {
+        //Privat variabel för klassen Car
+        private string _name;
+
+        //Skapar en publik string property med namnet Name, get {} returnerar variabeln _name.
+        //set{} sätter värdet _name till värdet som hämtas
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Level
+        {
+            get;
+            set;
+        }
+        private string _characterClass;
+        public string CharacterClass
+        {
+            get { return _characterClass; }
+            set { _characterClass = value; }
+        }
+        public bool Alive { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Your name: {Name}.\nYour level: {Level}.\nAre you alive: {Alive}");
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\n" +
+                $"Level: {Level}\n" +
+                $"Alive Status: {Alive} ";
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car player = new Car();
+
+            player.Level = 17;
+            player.Alive = true;
+            player.Name = "Toni";
+
+            player.PrintInfo();
+            Console.WriteLine(player.ToString());
+        }
+    }
+}
