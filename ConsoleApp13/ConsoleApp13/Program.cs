75a68077c7018d0dd5f4c90f6cbf49a7c35766a6
yousoezs <skorotoni@hotmail.com>
2022-09-22 11:37:41 +0200

    Uppgift 26

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        //Skapa en klass som kan användas som stegräknare.
+        //Den ska ha en property Steps som bara går att läsa; en metod Step() 
+        //som räknar upp Steps med 1 varje gång man anropar den; och en metod Reset() 
+        //som nollställer räknaren.
+
+        //Inputs... Integer that increments with every call.
+        //Complex inputs... method for reseting steps.
+        //Outputs... Every step the call makes, +1. Also Output for resetting.
+
+        //For every method call, incremenet 1. if Space is pushed down, reset counter.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var counter = new StepCounter();
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    counter.Step();
+                }
+                else if (key.Key == ConsoleKey.Spacebar)
+                {
+                    counter.Reset();
+                    break;
+                }
+            }
+            Console.WriteLine("Outside of loop now, bye!");
+        }
+    }
+}
---
+++ ConsoleApp13/ConsoleApp13/StepCounter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    //Skapa en klass som kan användas som stegräknare.
+    //Den ska ha en property Steps som bara går att läsa; en metod Step() 
+    //som räknar upp Steps med 1 varje gång man anropar den; och en metod Reset() 
+    //som nollställer räknaren.
+
+    //Inputs... Integer that increments with every call.
+    //Complex inputs... method for reseting steps.
+    //Outputs... Every step the call makes, +1. Also Output for resetting.
+    public class StepCounter
+    {
+        private int _steps;
+
+        public int Steps
+        {
+            get { return _steps; }
+            set { _steps = value; }
+        }
+
+        public void Step()
+        {
+            Steps++;
+            Console.WriteLine($"Count of steps: {Steps}");
+        }
+
+        public void Reset()
+        {
+            Steps = 0;
+            Console.WriteLine($"Count of steps reset, steps count: {Steps}");
+        }
+    }
+}
